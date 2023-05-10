3729c0b007431319903bcd14ebdcc47f72e07fac
morgan4080 <morganmurungi@live.com>
2023-05-10 15:54:12 +0300

    copied product selection

--- shared/src/commonMain/kotlin/composables/ProductSelectionCard.kt
+++ shared/src/commonMain/kotlin/composables/ProductSelectionCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import theme.backArrowColor
+import theme.containerColor
 import theme.labelTextColor
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,29 +28,29 @@ fun  productSelectionCard(label: String,description: String?=null, onClickContai
 
         ,
         modifier = Modifier.fillMaxWidth()
-            .background(color = MaterialTheme.colorScheme.background)
+            .background(color = containerColor)
             .padding(top = 10.dp)
     ) {
         Box(modifier = Modifier.background(color = Color.White)) {
             Row(
-            modifier = Modifier.padding(top = 9.dp, bottom = 9.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically){
+                modifier = Modifier.padding(top = 9.dp, bottom = 9.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically){
 
                 Column(){
 
                     Text(
                         text = label,
-                    modifier = Modifier.padding(start = 15.dp),
-                    fontSize = 15.sp,
-                    color = labelTextColor)
+                        modifier = Modifier.padding(start = 15.dp),
+                        fontSize = 15.sp,
+                        color = labelTextColor)
                     //Spacer(modifier = Modifier.weight(1f))
                     if (description != null) {
                         Text(
 
                             text = description,
                             modifier = Modifier.padding(start = 15.dp),
-                        fontSize = 12.sp,)
+                            fontSize = 12.sp,)
                     }
 
                 }
@@ -82,6 +83,3 @@ fun  productSelectionCard(label: String,description: String?=null, onClickContai
 
 
 }
-
-
-
