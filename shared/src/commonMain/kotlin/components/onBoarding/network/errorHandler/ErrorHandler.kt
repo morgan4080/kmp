ef3e8b91daa608030c9f564d2acc1a2def0d42e1
morgan4080 <morganmurungi@live.com>
2023-05-16 18:44:50 +0300

    configured networks auth error handler

---
+++ shared/src/commonMain/kotlin/components/onBoarding/network/errorHandler/ErrorHandler.kt
@@ -0,0 +1,34 @@
+package components.onBoarding.errorHandler
+
+import components.onBoarding.network.errors.OnBoardingError
+import components.onBoarding.network.errors.OnBoardingException
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+import io.ktor.utils.io.errors.IOException
+import kotlinx.coroutines.withContext
+import prestaDispatchers
+
+suspend inline fun <reified T> errorHandler(
+    crossinline response: suspend () -> HttpResponse
+): T = withContext(prestaDispatchers.io) {
+
+    val result = try {
+        response()
+    } catch(e: IOException) {
+        throw OnBoardingException(OnBoardingError.ServiceUnavailable)
+    }
+
+    when(result.status.value) {
+        in 200..299 -> Unit
+        in 400..499 -> throw OnBoardingException(OnBoardingError.ClientError)
+        500 -> throw OnBoardingException(OnBoardingError.ServerError)
+        else -> throw OnBoardingException(OnBoardingError.UnknownError)
+    }
+
+    return@withContext try {
+        result.body()
+    } catch(e: Exception) {
+        throw OnBoardingException(OnBoardingError.ServerError)
+    }
+
+}
